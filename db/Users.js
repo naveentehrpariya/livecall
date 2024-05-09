9bf37ef206f52cf55d7b075a8b6a19dec56d672b
Naveen Tehrpariya <naveen@internetbusinesssolutionsindia.com>
2024-05-09 18:31:05 +0530

    subscription process, payment process, stream limit based on subscription added

--- db/Users.js
+++ db/Users.js
@@ -5,115 +5,97 @@ const crypto = require('crypto');
 
 const schema = new mongoose.Schema({
     name: {
-        type:String, 
-        required:[true, 'Please enter your name.'],
+        type: String,
+        required: [true, 'Please enter your name.'],
     },
     username: {
-        required:[true, 'Please enter your username.'],
-        unique:[true, 'Username is already taken.'], 
-        type:String,
-        minLength:5,
-        maxLength:10
+        required: [true, 'Please enter your username.'],
+        unique: [true, 'Username is already taken.'],
+        type: String,
+        minLength: 5,
+        maxLength: 10
     },
     email: {
-        type:String,
-        unique:true, 
-        required:[true, 'Please enter your email address.'],
-        lowercase:true,
+        type: String,
+        unique: true,
+        required: [true, 'Please enter your email address.'],
+        lowercase: true,
         validate: [validator.isEmail, 'Please provide a valid email address.']
     },
     free_trial: {
         type: Date,
-        default: function() {
+        default: function () {
             const threeDaysFromNow = new Date();
             threeDaysFromNow.setDate(threeDaysFromNow.getDate() + 3);
-            this.freeTrialStatus = 'active';
             return threeDaysFromNow;
         }
     },
-    freeTrialStatus: {
-        type: String,
-        enum: ['active', 'ended'],
-        default: 'active'
-    },
     plan: { type: mongoose.Schema.Types.ObjectId, ref: 'pricings' },
     avatar: {
-        type:String,
+        type: String,
     },
     password: {
-        type:String,
-        required:[true, 'Please enter your password.'],
-        select:false
+        type: String,
+        required: [true, 'Please enter your password.'],
+        select: false
     },
     confirmPassword: {
-        type:String,
-        required:true,
-        required:[true, 'Please re enter your password.'],
-        select:false,
-        validate : {
-            validator : function (val) { return val === this.password },
-            message : 'Password did\'t matched.'
+        type: String,
+        required: true,
+        required: [true, 'Please re-enter your password.'],
+        select: false,
+        validate: {
+            validator: function (val) { return val === this.password },
+            message: 'Passwords do not match.'
         }
     },
-    active : { 
-        type:Boolean,
-        default:true,
-        select:false,
+    active: {
+        type: Boolean,
+        default: true,
+        select: false,
     },
     createdAt: {
         type: Date,
-        default: Date.now()     
+        default: Date.now()
     },
     changedPasswordAt: Date,
-    passwordResetToken : String,
-    resetTokenExpire : Date,
+    passwordResetToken: String,
+    resetTokenExpire: Date,
+}, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
 });
 
-schema.pre('save', async function(next) { 
-    if(!this.isModified('password')) return next();
+schema.virtual('trialStatus').get(function () {
+    const currentDate = new Date();
+    if (this.free_trial < currentDate) {
+        return 'ended';
+    } else {
+        return 'active';
+    }
+});
+
+schema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
     this.password = await bcrypt.hash(this.password, 12);
     this.confirmPassword = undefined;
 });
 
-schema.pre(/ˆfind/, async function(next) { 
-    this.find({active: {$ne:false}});    
+schema.pre(/^find/, function (next) {
+    this.find({ active: { $ne: false } });
+    next();
 });
 
-schema.methods.checkPassword = async function (pass, hash) { 
-   return await bcrypt.compare(pass, hash);
+schema.methods.checkPassword = async function (pass, hash) {
+    return await bcrypt.compare(pass, hash);
 }
 
-schema.methods.createPasswordResetToken  = async function() { 
+schema.methods.createPasswordResetToken = async function () {
     const token = crypto.randomBytes(32).toString('hex');
     this.passwordResetToken = crypto.createHash('sha256').update(token).digest('hex');
     this.resetTokenExpire = Date.now() + 10 * 60 * 1000;
     return token;
 }
 
-schema.methods.checkFreeTrialStatus = async function() {
-    if (this.freeTrialStatus === 'active') {
-        if (this.free_trial <= new Date()) {
-            this.freeTrialStatus = 'ended';
-            this.save();
-        }
-        return 'active';
-    } else {
-        return 'ended';
-    }
-}
-
-schema.pre(/ˆfind/, async function(next) { 
-    if (this.free_trial <= new Date()) {
-        this.freeTrialStatus = 'ended';
-        this.save();
-    } else {
-        this.freeTrialStatus = 'active';
-        this.save();
-    }
-});
-
-
 const User = mongoose.model('users', schema);
 module.exports = User;
-
- 
