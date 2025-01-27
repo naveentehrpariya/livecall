3b993f155edc816dae273bf722ad1a798c3a7b31
naveenfutureprofilez <naveen@internetbusinesssolutionsindia.com>
2025-01-27 17:36:53 +0530

    merge changes

--- utils/resolutionSettings.js
+++ utils/resolutionSettings.js
@@ -1,35 +1,35 @@
 const resolutionSettings = {
   '2160p': {
     resolution: '3840x2160',
-    videoBitrate: '12000k',
-    maxrate: '18000k',
-    bufsize: '24000k',
+    videoBitrate: '15000k',
+    maxrate: '22500k',
+    bufsize: '30000k',
     preset: 'medium',
-    gop: '120',
+    gop: '120', // 2 seconds at 60fps
   },
   '1080p': {
     resolution: '1920x1080',
-    videoBitrate: '4500k',
+    videoBitrate: '4000k',
     maxrate: '6000k',
-    bufsize: '9000k',
+    bufsize: '8000k',
     preset: 'fast',
-    gop: '60',
+    gop: '60', // 2 seconds at 30fps
   },
   '720p': {
     resolution: '1280x720',
-    videoBitrate: '2500k',
-    maxrate: '3500k',
-    bufsize: '5000k',
+    videoBitrate: '3000k',
+    maxrate: '4500k',
+    bufsize: '6000k',
     preset: 'faster',
-    gop: '60',
+    gop: '60', // 2 seconds at 30fps
   },
   '720x1080': {
     resolution: '720x1080',
-    videoBitrate: '3000k',
-    maxrate: '4000k',
-    bufsize: '5000k',
+    videoBitrate: '3500k',
+    maxrate: '5000k',
+    bufsize: '7000k',
     preset: 'fast',
-    gop: '60',
+    gop: '60', // 2 seconds at 30fps
   },
 };
 
